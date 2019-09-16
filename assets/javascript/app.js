4b0a8b99664be499103a1e20a2c6e83234170213
James Grodzicki <jamespgrodzicki@gmail.com>
2019-09-16 10:06:16 -0500

    added correct answer page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,17 @@
 var QandA = [
     {
         question: 'q1',
-        answer: ['a', 'b', 'c', 'd']
+        answer: ['a', 'b', 'c', 'd'],
+        correct: 2
     },
     {
         question: 'q2',
-        answer: ['a', 'b', 'c', 'd']
+        answer: ['a', 'b', 'c', 'd'],
+        correct: 1
     }
 ];
+var ifCorrect = ["https://media1.tenor.com/images/d6692229801e4fee03011d28e29665c0/tenor.gif?itemid=7603405"];
+var ifWrong = [];
 var questionNum = 0;
 var interval;
 var initialTime = 5;
@@ -34,12 +38,27 @@ function showQuestion(){
 
 function showAnswers(){
     for(var i=0; i<4;i++){
-        $(".buttons").append(`<button type="button" class="btn btn-light btn-lg btn-block">${QandA[questionNum].answer[i]}</button>`);
+        $(".button-container").append(`<button type="button" id="${i}" class="btn btn-light btn-lg btn-block buttons">${QandA[questionNum].answer[i]}</button>`);
     }
+}
+
+function correctScreen(){
+    $(".button-container").empty();
+    $("#gif-holder").append(`<img src="${ifCorrect[questionNum]}">`)
+    initialTime = 3;
+    timer();
 }
 
 timer();
 $(document).ready(function () {
     showQuestion();
     showAnswers();
+    $(".buttons").on("click", function(){
+        if($(this).attr("id") == QandA[questionNum].correct){
+            correctScreen();
+            questionNum++;
+            initialTime = 10;
+
+        }
+    });
 });
